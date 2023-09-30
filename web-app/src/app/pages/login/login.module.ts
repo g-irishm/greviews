dc172da28eaf6183036f7d1eda119e56413b85a9
Girish Mehta <girish.mehta100@gmail.com>
2023-09-30 11:48:10 +0530

    FEAT:14 Added login component

--- web-app/src/app/pages/login/login.module.ts
+++ web-app/src/app/pages/login/login.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginPage } from './login.page';
+import { LoginComponent } from 'src/app/components/login/login.component';
 
 
 @NgModule({
   declarations: [
-    LoginPage
+    LoginPage,
+    LoginComponent
   ],
   imports: [
     CommonModule,
